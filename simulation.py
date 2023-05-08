71b8246b33495ad4c79c6f879cc49039b9cea033
Glatzl André <glatzlandre@mercury.fhgr.ch>
2023-05-08 13:15:21 +0200

    final simulation

--- simulation.py
+++ simulation.py
@@ -4,20 +4,24 @@ from numba import jit
 import matplotlib.pyplot as plt
 import matplotlib.animation as anime
 from tqdm import tqdm
+import multiprocessing
+import time
 
-MAX_ITERATIONS = 100000
+MAX_ITERATIONS = int(30e6)
 
 """
 2017 A. R. Malipeddi
-A simple 2D geometric multigrid solver for the homogeneous Dirichlet Poisson problem on Cartesian grids and unit square. Cell centered 5-point finite difference operator.
+A simple 2D geometric multigrid solver on Cartesian grids and unit square.
 """
 
 import numpy as np
-from scipy.sparse.linalg import LinearOperator
 from numba import jit
 
 @jit(nopython=True, nogil=True, fastmath=True)
 def relax(vxo, vyo, po, ro, pn, rn, dt, dx, dy, dx2, dy2, rho, grid_size_x, grid_size_y, iters=1):
+    Ax=1.0/dx**2; Ay=1.0/dy**2
+    Ap=1.0/(2.0*(Ax+Ay))
+    Ap=1
     for _ in range(iters):
         vxo, vyo, po = update_boundaries(vxo, vyo, po)
         for i in range(1, grid_size_x - 1):
@@ -28,7 +32,7 @@ def relax(vxo, vyo, po, ro, pn, rn, dt, dx, dy, dx2, dy2, rho, grid_size_x, grid
                 RHS_C = 2*dvxo*dvyo
                 RHS_R = dvyo**2
                 RHS = rho*((1/(dt)) * (dvxo + dvyo ) - RHS_L - RHS_C - RHS_R )
-                pn[i,j] = (-1)*(dx2*dy2/(dx2 + dy2))*( RHS - (po[i+1,j]/2*dx2) - (po[i-1,j]/2*dx2) - (po[i,j+1]/2*dy2) - (po[i,j-1]/2*dy2) )
+                pn[i,j] = Ap*((-1)*(dx2*dy2/(dx2 + dy2))*( RHS - (po[i+1,j]/2*dx2) - (po[i-1,j]/2*dx2) - (po[i,j+1]/2*dy2) - (po[i,j-1]/2*dy2) ))
                 rn[i,j] = ro[i,j] + (pn[i,j] - po[i,j]) * dt
         vxo, vyo, pn = update_boundaries(vxo, vyo, pn)
     return pn, rn
@@ -39,11 +43,6 @@ def restrict(nx,ny,v):
   restrict 'v' to the coarser grid
   '''
   v_c=np.zeros((nx+2,ny+2))
-
-#  #vectorized form of 
-#  for i in range(1,nx+1):
-#    for j in range(1,ny+1):
-#      v_c[i,j]=0.25*(v[2*i-1,2*j-1]+v[2*i,2*j-1]+v[2*i-1,2*j]+v[2*i,2*j])
   
   v_c[1:nx+1,1:ny+1]=0.25*(v[1:2*nx:2,1:2*ny:2]+v[1:2*nx:2,2:2*ny+1:2]+v[2:2*nx+1:2,1:2*ny:2]+v[2:2*nx+1:2,2:2*ny+1:2])
 
@@ -55,14 +54,6 @@ def prolong(nx,ny,v):
   interpolate 'v' to the fine grid
   '''
   v_f=np.zeros((2*nx+2,2*ny+2))
-
-#  #vectorized form of 
-#  for i in range(1,nx+1):
-#    for j in range(1,ny+1):
-#      v_f[2*i-1,2*j-1] = 0.5625*v[i,j]+0.1875*(v[i-1,j]+v[i,j-1])+0.0625*v[i-1,j-1]
-#      v_f[2*i  ,2*j-1] = 0.5625*v[i,j]+0.1875*(v[i+1,j]+v[i,j-1])+0.0625*v[i+1,j-1]
-#      v_f[2*i-1,2*j  ] = 0.5625*v[i,j]+0.1875*(v[i-1,j]+v[i,j+1])+0.0625*v[i-1,j+1]
-#      v_f[2*i  ,2*j  ] = 0.5625*v[i,j]+0.1875*(v[i+1,j]+v[i,j+1])+0.0625*v[i+1,j+1]
 
   a=0.5625; b=0.1875; c= 0.0625
 
@@ -83,7 +74,7 @@ def V_cycle(vxo, vyo, po, ro, pn, rn, dt, length_x, length_y, rho, grid_size_x, 
   V cycle
   '''
   if(level==num_levels):#bottom solve
-    u,res=relax(vxo, vyo, po, ro, pn, rn, dt, dx, dy, dx2, dy2, rho, grid_size_x, grid_size_y, iters=200)
+    u,res=relax(vxo, vyo, po, ro, pn, rn, dt, dx, dy, dx2, dy2, rho, grid_size_x, grid_size_y, iters=50)
     return u,res
 
   #Step 1: Relax Au=f on this grid
@@ -158,6 +149,12 @@ def solve_navier_stokes(vxo, vyo, po, ro, vxn, vyn, pn, rn, dt, dx, dy, dx2, dy2
 
     return vxn, vyn, pn, rn, diffs
 
+def save_results(length_x, length_y, grid_size_x, grid_size_y, dt, dx, dy, dx2, dy2, rho, nu, min_tol, max_tol, vxo, vyo, po, ro, vxn, vyn, pn, rn, t, tot_p, tot_v, iterations, diffs, p_diffs):
+    vxo, vyo, po = update_boundaries(vxo, vyo, po)
+    vars = [length_x, length_y, grid_size_x, grid_size_y, dt, dx, dy, dx2, dy2, rho, nu, min_tol, max_tol, vxo, vyo, po, ro, vxn, vyn, pn, rn, t, tot_p, tot_v, iterations, diffs, p_diffs]
+    with open('simulation.pickle', 'wb') as f:
+        pickle.dump(vars, f)
+
 # 2. Main Loop
 # simulation main loop
 def run_simulation():
@@ -171,7 +168,7 @@ def run_simulation():
     cinematic_viscosity = 0.01 # m^2/s
     min_tolerance = 1e-15
     max_tolerance = 1e15
-    time_step = 0.0001 # s # Durch herausprobieren herausgefunden
+    time_step = 0.00001 # s # Durch herausprobieren herausgefunden
     grid_vx_old = np.zeros((grid_size_x, grid_size_y))
     grid_vy_old = np.zeros((grid_size_x, grid_size_y))
     grid_p_old = np.zeros((grid_size_x, grid_size_y))
@@ -205,6 +202,8 @@ def run_simulation():
     p_diffs = []
 
     pbar = tqdm(range(MAX_ITERATIONS))
+    last_time = time.time()
+    pool = multiprocessing.Pool()
     for i in pbar:
         vxo, vyo, po = update_boundaries(vxo, vyo, po)
         vxn, vyn, pn, rn, p_diff = solve_navier_stokes(vxo, vyo, po, ro, vxn, vyn, pn, rn, dt, dx, dy, dx2, dy2, length_x, length_y, rho, nu, grid_size_x, grid_size_y, min_tol, max_tol)
@@ -224,6 +223,11 @@ def run_simulation():
         vxo, vyo, po = vxn.copy(), vyn.copy(), pn.copy()
         iterations += 1
         pbar.set_description(f"Pressure residual: {p_diff:.2E}, Velocity residual: {diff:.2E}")
+        # check if 60 seconds have passed
+        if time.time() - last_time >= 60:
+            last_time = time.time()
+            pool.apply_async(save_results, args=(length_x, length_y, grid_size_x, grid_size_y, dt, dx, dy, dx2, dy2, rho, nu, min_tol, max_tol, vxo, vyo, po, ro, vxn, vyn, pn, rn, t, tot_p, tot_v, iterations, diffs, p_diffs))
+    pool.close()
 
     if iterations == MAX_ITERATIONS:
         print("Warning: Velocity solver did not converge after MAX_ITERATIONS iterations")
@@ -232,11 +236,7 @@ def run_simulation():
     print(f"Pressure residual: {p_diffs[-1]}")
     print(f"Velocity residual: {diffs[-1]}")
 
-    vxo, vyo, po = update_boundaries(vxo, vyo, po)
-    vars = [length_x, length_y, grid_size_x, grid_size_y, dt, dx, dy, dx2, dy2, rho, nu, min_tol, max_tol, vxo, vyo, po, ro, vxn, vyn, pn, rn, t, tot_p, tot_v, iterations, diffs, p_diffs]
-    print(f"Total iterations: {iterations}")
-    with open('simulation.pickle', 'wb') as f:
-        pickle.dump(vars, f)
+    save_results(length_x, length_y, grid_size_x, grid_size_y, dt, dx, dy, dx2, dy2, rho, nu, min_tol, max_tol, vxo, vyo, po, ro, vxn, vyn, pn, rn, t, tot_p, tot_v, iterations, diffs, p_diffs)
 
 if __name__ == "__main__": 
     run_simulation()
